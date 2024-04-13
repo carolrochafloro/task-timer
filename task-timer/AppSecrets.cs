0b9ea39247d6f552cacca344fae94ab8aecf6062
Carolina Floro <carolinarochaf@hotmail.com>
2024-04-12 21:46:28 -0300

    Updated AppSecrets.cs with JWT variables.

--- task-timer/AppSecrets.cs
+++ task-timer/AppSecrets.cs
@@ -2,12 +2,18 @@
 {
     public class AppSecrets
     {
-
+        // database
         public string host {  get; set; }
         public int port { get; set; }
         public string database { get; set; }
         public string key { get; set; }
         public string password { get; set; }
+        
+        // jwt
+        public string issuer { get; set; }
+        public string audience { get; set; }
+        public string secretKey { get; set; }
+
   
     }
 }
