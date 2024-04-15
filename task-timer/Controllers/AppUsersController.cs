8c9e1d7ae6fe82fd405090dbe406db6f251afd45
Carolina Floro <carolinarochaf@hotmail.com>
2024-04-15 16:09:20 -0300

    Update error message in UserController's Post method

--- task-timer/Controllers/AppUsersController.cs
+++ task-timer/Controllers/AppUsersController.cs
@@ -1,13 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using task_timer.Context;
 using task_timer.Models;
-using BCrypt.Net;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace task_timer.Controllers;
 
@@ -40,13 +39,29 @@ public class AppUsersController : ControllerBase
             return BadRequest("All data must be provided.");
         }
 
+        var dbUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == user.Email);
+
+        if (dbUser != null)
+        {
+            return BadRequest("This e-mail already is already registered.");
+        }
+
         // Using bcrypt to hash the password
         user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
 
-        _context.Add(user);
-        await _context.SaveChangesAsync();
+        try
+        {
+            _context.Add(user);
+            await _context.SaveChangesAsync();
 
-        return Ok("User registered.");
+            return Ok("User registered.");
+        }
+        catch (Exception)
+        {
+
+            return BadRequest("Internal server error.");
+        }
+
 
     }
 
@@ -58,24 +73,27 @@ public class AppUsersController : ControllerBase
         // generate token
 
         var user = await _context.Users.SingleOrDefaultAsync(u => u.Email == email);
-        if (user is null)
-        {
-            return Unauthorized("User or password are invalid.");
-        }
 
-        bool validation = BCrypt.Net.BCrypt.Verify(password, user.Password);
-
-        if (!validation)
+        try
         {
-            return Unauthorized("User or password are invalid.");
-        }
+            if (user is null)
+            {
+                return Unauthorized("User or password are invalid.");
+            }
 
-        // Definir os claims do token
-        var claims = new[]
-        {
+            bool validation = BCrypt.Net.BCrypt.Verify(password, user.Password);
+
+            if (!validation)
+            {
+                return Unauthorized("User or password are invalid.");
+            }
+
+            // Definir os claims do token
+            var claims = new[]
+            {
             new Claim(JwtRegisteredClaimNames.Sub, user.Email),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-};
+        };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["jwtSettings:secretKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -87,9 +105,42 @@ public class AppUsersController : ControllerBase
                 expires: DateTime.Now.AddMinutes(30),
                 signingCredentials: creds);
 
-        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+        }
+        catch (Exception)
+        {
+
+            return BadRequest("Internal server error.");
+        }
+
 
     }
 
+    //[Authorize]
+    [HttpPut("{id:int:min(1)}")]
+    public async Task<IActionResult> PutAsync(AppUser user)
+    {
+        // se trocar senha, fazer hash. encontrar user pelo bearer token,
+        // verificar se cada item foi enviado e, se sim, fazer a troca
 
+        // receber id do usuário pelo jwt
+
+        var previousUser = await _context.Users.SingleOrDefaultAsync(u => u.Email == user.Email);
+
+        try
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User data has been successfully updated.");
+        }
+        catch (Exception)
+        {
+            return BadRequest("Internal server error.");
+        }
+
+    }
 }
+
+
