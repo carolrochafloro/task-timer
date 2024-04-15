c99902515f8b2b5c5b67ddb47e467a9c65c59031
Carolina Floro <carolinarochaf@hotmail.com>
2024-04-15 16:09:55 -0300

    Created post method on CategoriesController.

--- task-timer/Controllers/CategoriesController.cs
+++ task-timer/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using task_timer.Context;
 using task_timer.Models;
 
@@ -17,11 +18,41 @@ public class CategoriesController : ControllerBase
     }
 
     [HttpGet]
-
-    public ActionResult<IEnumerable<Category>> Get()
+    public async Task<ActionResult<IEnumerable<Category>>> Get()
     {
-        var categories = _context.Categories.ToList();
+        // get authentication and authorization to allow the user to 
+        // only access the categories he created
+
+        var categories = await _context.Categories.ToListAsync();
         return categories;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post(Category category)
+    {
+        var dbCategory = await _context.Categories.FirstOrDefaultAsync(c => c.Name == category.Name);
+
+        if (category is null)
+        {
+            return BadRequest("All data must be provided.");
+        }
+
+        if (dbCategory != null)
+        {
+            return BadRequest("This category already exists.");
+        }
+
+        try
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return Ok(category);
+        }
+
+        catch (Exception)
+        {
+            return BadRequest("Internal server error.");
+        }
     }
 
 }
