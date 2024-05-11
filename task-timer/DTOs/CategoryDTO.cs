2e4f8568628a87875efea9aa56c0ca497e427275
Carolina Floro <carolinarochaf@hotmail.com>
2024-05-11 12:49:45 -0300

    <chore> Removed [required] from categorydto description

---
+++ task-timer/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace task_timer.DTOs;
+
+public class CategoryDTO
+{
+    [Required]
+    [MaxLength(200)]
+    public string? Name { get; set; }
+
+    [MaxLength(400)]
+    public string? Description { get; set; }
+
+    public string? ImgUrl { get; set; }
+}
